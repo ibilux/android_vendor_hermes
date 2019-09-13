42a50fb32aa90f264aa32127e87068fd0bd694ad
bilux <i.bilux@gmail.com>
2019-09-13 11:01:43 +0100

    vendor: use M blobs based on Vanzo Vendor
    Update vendor with android M blobs based on Vanzo Vendor (ALPS-MP-M0.MP11-V1_VZ6795_LWT_M)
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
-CAL_PATH := $(call my-dir)
-
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -21,4 +20,14 @@ LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libvcodecdrv.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libvcodecdrv.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libion_mtk.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libion_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
