1026068013a40d0f068c84a587f89044dd481410
bilux <i.bilux@gmail.com>
2019-11-27 15:29:02 +0100

    Add common dependencies
    Signed-off-by: bilux <i.bilux@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_OWNER := mtk
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE = true
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
@@ -14,9 +14,9 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvcodecdrv
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_OWNER := mtk
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE = true
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libvcodecdrv.so
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libvcodecdrv.so
@@ -30,4 +30,45 @@ LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam.utils.sensorlistener
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_OWNER := mtk
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libcam.utils.sensorlistener.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libcam.utils.sensorlistener.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libged
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libged.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libged.so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := librilmtk
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/librilmtk.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/librilmtk.so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mtk-ril
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/mtk-ril.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/mtk-ril.so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
