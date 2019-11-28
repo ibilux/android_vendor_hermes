6a5718c0209618180523808bb72629343a3f4a65
bilux <i.bilux@gmail.com>
2019-11-28 08:53:10 +0100

    Move libdpframework dependencies from ".mk" to ".bp"
    Signed-off-by: bilux <i.bilux@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,17 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libdpframework
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER := mtk
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libvcodecdrv
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_OWNER := mtk
